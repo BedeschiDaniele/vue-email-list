5a85d909ad1899acee546941671259090b8f6928
Daniele <daniele.bedeschi94@gmail.com>
2020-12-16 15:58:53 +0100

    Commit Finale

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ var app = new Vue(
     for (var i = 0; i < 10; i++) {
       axios
       .get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(function (result) {
-       app.randomEmails.push(result.data.response);
+      .then((result) => {
+       this.randomEmails.push(result.data.response);
        });
     }
 
